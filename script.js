4300880492b842d95854c479a0e6640450700c46
mikhailkonkov <mkonsmm@gmail.com>
2026-02-25 23:36:08 +0300

    adding punctuation marks

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ const recolorFirstCardButton = document.getElementById('recolor-first-card-butto
 const firstProductCard = document.querySelector('.product-card');
 const charcoalHashColor = '#36454F';
 
-recolorFirstCardButton.addEventListener('click',() => {
+recolorFirstCardButton.addEventListener('click', () => {
   firstProductCard.style.backgroundColor = charcoalHashColor;
-})
+});
 
 //Кнопка по покраске фона всех карточек
 
@@ -14,22 +14,22 @@ const recolorAllCardButton = document.getElementById('recolor-all-card-button');
 const productCards = document.querySelectorAll('.product-card');
 const sandHashColor = '#E3BC9A';
 
-recolorAllCardButton.addEventListener('click',() => {
+recolorAllCardButton.addEventListener('click', () => {
   productCards.forEach(
     card => card.style.backgroundColor = sandHashColor
-  )
-})
+  );
+});
 
 //Кнопка по переходу в Google
 
 const googleOpenButton = document.getElementById('open-google-button');
-googleOpenButton.addEventListener('click',openGoogle)
+googleOpenButton.addEventListener('click',openGoogle);
 
 function openGoogle() {
-  const answer = confirm('Вы действительно хотите перейти на сайт Google?')
+  const answer = confirm('Вы действительно хотите перейти на сайт Google?');
 
-  if(answer === true) {
-    window.open('http://google.com')
+  if (answer === true) {
+    window.open('http://google.com');
   }
 }
 
@@ -37,14 +37,14 @@ function openGoogle() {
 
 const mainTitle = document.querySelector('.top-title');
 
-mainTitle.addEventListener('mouseover',() => {
-  console.log(mainTitle.textContent)
-})
+mainTitle.addEventListener('mouseover', () => {
+  console.log(mainTitle.textContent);
+});
 
 //Смена цвета кнопки при нажатии
 
-const changeColorButton = document.querySelector('.change-color-button')
+const changeColorButton = document.querySelector('.change-color-button');
 
 changeColorButton.addEventListener('click', () => {
-  changeColorButton.classList.toggle('active')
-})
+  changeColorButton.classList.toggle('active');
+});
