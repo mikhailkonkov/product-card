103517b93ac93a00ecedcd6a1f0562cc66c31b9b
mikhailkonkov <mkonsmm@gmail.com>
2026-07-26 15:36:47 +0300

    footer and modal

--- productcards.js
+++ productcards.js
@@ -1,7 +1,7 @@
 export const productCards = [
   {
     id: 1,
-    imgSrc: "/images/moisturizing-mousse.png", 
+    imgSrc: "images/moisturizing-mousse.png", 
     forSkin: "для нормальной кожи",
     title: "Увлажняющий мусс",
     description: "Глубоко увлажняет кожу лица, оставляя её мягкой и гладкой.",
@@ -14,7 +14,7 @@ export const productCards = [
   },
     {
     id: 2,
-    imgSrc: "/images/moisturizing-mask.png",
+    imgSrc: "images/moisturizing-mask.png",
     forSkin: "для нормальной кожи",
     title: "Увлажняющая маска",
     description: "Способствует удерживанию влаги в верхних слоях кожи.",
@@ -27,7 +27,7 @@ export const productCards = [
   },
     {
     id: 3,
-    imgSrc: "/images/cleansing-gel.png", 
+    imgSrc: "images/cleansing-gel.png", 
     forSkin: "для нормальной кожи",
     title: "Гель для умывания",
     description: "Интенсивно очищает, не повреждает защитный барьер кожи.",
@@ -40,7 +40,7 @@ export const productCards = [
   },
     {
     id: 4,
-    imgSrc: "/images/gift-set-one.png", 
+    imgSrc: "images/gift-set-one.png", 
     forSkin: "для нормальной кожи",
     title: "Подарочный набор",
     description: "Набор, состоящий из увлажняющего крема и маски.",
@@ -53,7 +53,7 @@ export const productCards = [
   },
     {
     id: 5,
-    imgSrc: "/images/gift-set-five.png", 
+    imgSrc: "images/gift-set-five.png", 
     forSkin: "для нормальной кожи",
     title: "Подарочный набор № 5",
     description: "Весь набор средств Invisible symphony, крем, маска, мусс и гель для умывания.",
